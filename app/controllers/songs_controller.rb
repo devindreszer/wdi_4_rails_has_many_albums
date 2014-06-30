674d152b37e5bbdb9a104c17198d7655871ea831
Devin Dreszer <devindreszer@gmail.com>
2014-06-30 15:51:12 -0400

    Songs use a before_action

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,13 +1,21 @@
 class SongsController < ApplicationController
 
+  # Run before the actions
+  before_action :set_album
+
   # Get /albums/:album_id/songs
   def index
-    @album = Album.find(params[:album_id])
     @songs = @album.songs
   end
 
   def show
-    @song = Song.find(params[:id])
+    @song = @album.songs.find(params[:id])
+  end
+
+  private
+
+  def set_album
+    @album = Album.find(params[:album_id])
   end
 
 end
