9b4ddcbe0aa16d19c077ab9d6d0190542020c3f3
Devin Dreszer <devindreszer@gmail.com>
2014-06-30 11:56:13 -0400

    Albums/authors have a dependent destroy option for songs/books

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,3 +1,4 @@
 class Album < ActiveRecord::Base
-  has_many :songs
+  # dependent: :destroy - will destroy, or remove, all child objects in memory and in the db
+  has_many :songs, dependent: :destroy
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,22 @@
 Album.delete_all
+Song.delete_all
 
-nevermind = Album.create(name: "Nevermind")
 sea_change = Album.create(name: "Sea Change")
+sea_change.songs.create!(title: 'Golden Age', artist: "Beck", price: 1.99, duration: 215)
+sea_change.songs.create!(title: 'Lost Cause', artist: "Beck", price: 4.99, duration: 182)
+sea_change.songs.create!(title: 'Lonesome Tears', artist: "Beck", price: 2.99, duration: 156)
 
-# nevermind.songs << Song.create(title: "Smells like Teen Spirit")
-# nevermind.songs << Song.create(title "In Bloom")
-# nevermind.songs << Song.create(title "Lithium")
+nevermind = Album.create(name: 'Nevermind')
+nevermind.songs.create!(title: 'Lithium', artist: "Nirvana", price: 1.99, duration: 215)
+nevermind.songs.create!(title: 'Come as you are', artist: "Nirvana", price: 4.99, duration: 182)
 
-# sea_change.songs << Song.create(title: "Golden Age"
-# sea_change.songs << Song.create(title "Lost Cause")
-# sea_change.songs << Song.create(title "Lonesome Tears")
+Author.delete_all
+Book.delete_all
 
+stephen_king = Author.create(name: "Stephen King")
+stephen_king.books.create!(title: 'It', price: 10.99, published_on: '1986-09-01')
+stephen_king.books.create!(title: '11/22/63', price: 12.99, published_on: '2011-11-08')
 
+jk_rowling = Author.create(name: "JK Rowling")
+jk_rowling.books.create!(title: 'Harry Potter and the Sorcerers Stone', price: 15.00, published_on: '1997-06-26')
+jk_rowling.books.create!(title: 'Harry Potter and the Chamber of Secrets', price: 15.00, published_on: '1998-07-02')
