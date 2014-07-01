08df42f2fca08aaf42c8af0fdbd991d2c51eb9cd
Devin Dreszer <devindreszer@gmail.com>
2014-07-01 10:25:03 -0400

    Signed-in users can create a new song and album

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,4 +1,5 @@
 class SongsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
   # Run before the actions
   before_action :set_album
@@ -12,10 +13,37 @@ class SongsController < ApplicationController
     @song = @album.songs.find(params[:id])
   end
 
+  def new
+    @song = Song.new
+  end
+
+  def create
+    @song = @album.songs.new(song_params)
+    if @song.save
+      redirect_to album_songs_path(@album, @song), notice: "You created a new song."
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
   private
 
   def set_album
     @album = Album.find(params[:album_id])
+  end
+
+  # strong params
+  def song_params
+    params.require(:song).permit(:title, :artist, :duration, :price)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   devise_for :users
   resources :albums, only: [:index, :show] do
-    resources :songs, only: [:index, :show]
+    resources :songs
   end
 
   resources :authors, only: [:index, :show] do
